92d7a761495b9d38c62e471ff8e8ab8b03831d51
Anthony Viard <anth.viard@gmail.com>
2021-12-08 16:09:03 +0100

    Fix card-config basic test

--- banking-plugin/ui/widgets/banking-widgets/dashboard-card-config/src/App.test.js
+++ banking-plugin/ui/widgets/banking-widgets/dashboard-card-config/src/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders all card choices', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const checkingElement = getByText(/Checking/i);
+  const creditCardElement = getByText(/Credit Card/i);
+  const savingsElement = getByText(/Savings/i);
+  expect(checkingElement).toBeInTheDocument();
+  expect(creditCardElement).toBeInTheDocument();
+  expect(savingsElement).toBeInTheDocument();
 });
