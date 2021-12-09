205d07c23a2043ccd9c34e0457a1f5fcf817fbc2
Anthony Viard <anth.viard@gmail.com>
2021-12-09 12:49:31 +0100

    fix injections and import for mocking the module

--- banking-plugin/ui/widgets/banking-widgets/dashboard-card-angular/src/app/card/card/card.component.spec.ts
+++ banking-plugin/ui/widgets/banking-widgets/dashboard-card-angular/src/app/card/card/card.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { CardService } from './card.service';
+import {TranslateModule} from '@ngx-translate/core';
+import {PipesModule} from '../../core/pipes/pipes.module';
+import {Observable} from 'rxjs';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -8,6 +12,14 @@ describe('CardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers: [{
+         provide: CardService,
+         useValue: jasmine.createSpyObj('CardService', {
+           getSeedsCardByUserID: new Observable(),
+           getSeedscard: new Observable(),
+         })
+      }],
+      imports: [TranslateModule.forRoot(), PipesModule],
       declarations: [ CardComponent ]
     })
     .compileComponents();
@@ -16,6 +28,11 @@ describe('CardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    component.keycloak = {
+      idTokenParsed: {
+        preferred_username: 'sampleUserName'
+      }
+    };
     fixture.detectChanges();
   });
 
