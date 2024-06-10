8f6ab15cef1e436ee941c799dd70b7c62bd044b7
Nathan Shaw <n.shaw@entando.com>
2024-06-10 14:06:57 -0400

    SD-35 Make initial changes to inject config

--- sd-banking/microfrontends/sd-seeds-card-angular/src/environments/environment.prod.ts
+++ sd-banking/microfrontends/sd-seeds-card-angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   domainUrl: '',
-  assetsPath: '/entando-de-app/cmsresources/sd-banking/static/dashboard-card-angular/assets'
+  assetsPath: '/entando-de-app/cmsresources/sd-banking/static/sd-seeds-card-angular/assets'
 };
