e4973cb0dffba84dd1cadcc65c1283ed35577f1b
Nathan Shaw <n.shaw@entando.com>
2024-06-12 14:32:56 -0400

    SD-37 Start upgrading manage users MFE

---
+++ manage-users/ui/widgets/manageusers/tableWidget/build/precache-manifest.7c7a8424f6eb20ffded8af25a3124829.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63674c8e0ddbc5720787ad32a8e4e90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc80812fc07b4521eb27",
+    "url": "/static/js/2.cf5477df.chunk.js"
+  },
+  {
+    "revision": "f82f3ba815ea554c7f86",
+    "url": "/static/js/main.17fbf2ca.chunk.js"
+  },
+  {
+    "revision": "0778c6b88810c6eaa04c",
+    "url": "/static/js/runtime~main.6d24a6fd.js"
+  }
+]);
